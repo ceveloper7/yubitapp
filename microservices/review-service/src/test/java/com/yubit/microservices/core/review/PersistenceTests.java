b3590b674b163c0a396a335e91c58ad30866057a
Carlos V <cevabarcvilla@gmail.com>
2024-07-29 23:54:05 -0300

    eCommerce Yubit App - Database testt using TestContainer

---
+++ microservices/review-service/src/test/java/com/yubit/microservices/core/review/PersistenceTests.java
@@ -0,0 +1,42 @@
+package com.yubit.microservices.core.review;
+
+import com.yubit.microservices.core.review.persistence.ReviewEntity;
+import com.yubit.microservices.core.review.persistence.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+//levanta base de datos embebida
+@DataJdbcTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PersistenceTests extends MySqlTestBase{
+    @Autowired
+    private ReviewRepository repository;
+    private ReviewEntity savedEntity;
+
+    @BeforeEach
+    void setUp(){
+        repository.deleteAll();
+        ReviewEntity entity = new ReviewEntity(1, 2, "a", "s", "c");
+        savedEntity = repository.save(entity);
+
+        assertEqualsReview(entity, savedEntity);
+    }
+
+    private void assertEqualsReview(ReviewEntity expectedEntity, ReviewEntity actualEntity) {
+        assertEquals(expectedEntity.getId(),        actualEntity.getId());
+        assertEquals(expectedEntity.getVersion(),   actualEntity.getVersion());
+        assertEquals(expectedEntity.getProductId(), actualEntity.getProductId());
+        assertEquals(expectedEntity.getReviewId(),  actualEntity.getReviewId());
+        assertEquals(expectedEntity.getAuthor(),    actualEntity.getAuthor());
+        assertEquals(expectedEntity.getSubject(),   actualEntity.getSubject());
+        assertEquals(expectedEntity.getContent(),   actualEntity.getContent());
+    }
+}
